514a2111ddeced472a355e2250ac72ff04b182c6
Christian Lopes <chrtannus@gmail.com>
2026-07-23 17:05:25 -0300

    Adds the `dist-test` folder to .gitignore and removes it from Git's tracking cache.

--- dist-test/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = void 0;
-var NetworkAnalyzerApp_1 = require("./NetworkAnalyzerApp");
-Object.defineProperty(exports, "default", { enumerable: true, get: function () { return NetworkAnalyzerApp_1.NetworkAnalyzerApp; } });
